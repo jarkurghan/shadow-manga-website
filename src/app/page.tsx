2de6bb561de6cbcf82d3e61471706616017d3129
Nazirov <jarkurghan@gmail.com>
2025-08-07 12:29:06 +0500

    change bot source

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
                 <div className="flex flex-col bg-slate-100 min-h-screen justify-center items-center gap-4 text-center">
                     <h1 className="text-2xl">Shadow Manga</h1>
                     <p className="max-w-sm">Dastur va website tayyorlanish jarayonida! Hozircha botimizda anime tomosha qilishga taklif qilamiz :)</p>
-                    <Link href={"https://t.me/aniuz_bot?start=channel"} target="_blank">
+                    <Link href={"https://t.me/aniuz_bot?start=website"} target="_blank">
                         <button type="button" className="flex justify-center w-xs items-center gap-4 h-10 text-white bg-blue-500 hover:bg-blue-600 transition rounded-lg cursor-pointer">
                             <TgIcon /> Botga o'tish
                         </button>
